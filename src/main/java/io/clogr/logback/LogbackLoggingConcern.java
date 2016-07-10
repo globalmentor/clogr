5f6c72c2157846e275ca68fbd2e78979554416e9
Garret Wilson <garret@globalmentor.com>
2016-07-10 15:17:55 -0300

    Added ability of Logback logging concern to perform auto initialization.

--- src/main/java/io/clogr/logback/LogbackLoggingConcern.java
+++ src/main/java/io/clogr/logback/LogbackLoggingConcern.java
@@ -22,6 +22,8 @@ import org.slf4j.*;
 import org.slf4j.event.Level;
 
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
 import io.clogr.LoggingConcern;
 
 /**
@@ -78,6 +80,19 @@ public class LogbackLoggingConcern extends LoggerContext implements LoggingConce
 			default:
 				throw new AssertionError("Unknown log level: " + level);
 		}
+	}
+
+	/**
+	 * Automatically configures this logging concern.
+	 * <p>
+	 * This automatic configuration is equivalent to the configuration performed by a default Logback installation independent of Clogr.
+	 * </p>
+	 * @return This logging concern; useful for chaining configuration commands.
+	 * @throws JoranException if an error occurred during configuration;
+	 */
+	public LogbackLoggingConcern autoConfig() throws JoranException {
+		new ContextInitializer(this).autoConfig();
+		return this;
 	}
 
 }
