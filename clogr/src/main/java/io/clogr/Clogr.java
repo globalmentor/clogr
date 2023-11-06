37c961a6d56cfab8692199f8ec1cfa7e516c8393
Garret Wilson <garret@globalmentor.com>
2023-11-06 17:00:02 -0300

    CLOGR-14: Added a facility for setting the system default logging provider.

--- clogr/src/main/java/io/clogr/Clogr.java
+++ clogr/src/main/java/io/clogr/Clogr.java
@@ -16,12 +16,16 @@
 
 package io.clogr;
 
+import static java.io.OutputStream.*;
+
+import java.io.PrintStream;
 import java.util.*;
 
 import javax.annotation.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.SLF4JServiceProvider;
 
 import io.csar.*;
 
@@ -87,6 +91,84 @@ public class Clogr {
 	 */
 	public static @Nonnull Logger getLogger(@Nonnull final Class<?> contextClass) {
 		return getLoggingConcern().getLogger(contextClass);
+	}
+
+	/**
+	 * Sets the preferred system default SLF4J provider to be used. If SLF4J provider initialization has already occurred, this method has no effect.
+	 * <p>
+	 * This default logging provider will only be used by Clogr logging concerns such as {@link LoggingConcern#DEFAULT} that access SLF4J directly, and by code
+	 * that accesses SLF4J without being aware of Clogr. This setting has no effect on Clogr logging concerns that do not use the system default.
+	 * </p>
+	 * @apiNote This method is useful for specifying a particular SLF4J logging provider to use if multiple providers are available on the classpath; or
+	 *          explicitly setting the logging provider so that the classpath will not be scanned, in order to lower startup times; or supplying a provider to
+	 *          use, such as a NOP provider, if no provider is on the classpath at all.
+	 * @apiNote This method sets the default SLF4J provider only if it has not already been set. If a different system default logging provider has been
+	 *          specified, this method will make no changes but will log a warning. This is useful for a program to set the system default logging provider but
+	 *          still allow it to be overridden by explicitly setting the system property externally. To set the default system logging provider invariably, even
+	 *          if it has already been set, call {@link #setSystemDefaultLoggingProvider(Class, boolean)} and specify a <code><var>force</var></code> value of
+	 *          <code>true</code>.
+	 * @implSpec This implementation sets the <code>slf4j.provider</code> system property by delegating to
+	 *           {@link #setSystemDefaultLoggingProvider(Class, boolean)} with a <code><var>force</var></code> value of <code>false</code>.
+	 * @implNote This implementation immediately "locks in" the new value by initializing SLF4J, so that future calls to this method will have no effect on the
+	 *           default provider actually used. This is done to suppress an output to <code>stderr</code> noting that an explicit provider is being used. This
+	 *           behavior may be removed when SLF4J discontinues sending such a message to <code>stderr</code>; see
+	 *           <a href="https://github.com/qos-ch/slf4j/issues/361">#361: Don't log to stderr when slf4j.provider is used.</a>.
+	 * @param loggingProviderClass The class of the default SLF4J logging provider to use.
+	 * @see <a href="https://jira.qos.ch/browse/SLF4J-450">SLF4J-450: Allow binding to be explicitly specified</a>
+	 */
+	public static void setSystemDefaultLoggingProvider(@Nonnull final Class<? extends SLF4JServiceProvider> loggingProviderClass) {
+		setSystemDefaultLoggingProvider(loggingProviderClass, false);
+	}
+
+	/**
+	 * Sets the system default SLF4J provider to be used. If SLF4J provider initialization has already occurred, this method has no effect.
+	 * <p>
+	 * This default logging provider will only be used by Clogr logging concerns such as {@link LoggingConcern#DEFAULT} that access SLF4J directly, and by code
+	 * that accesses SLF4J without being aware of Clogr. This setting has no effect on Clogr logging concerns that do not use the system default.
+	 * </p>
+	 * @apiNote This method is useful for specifying a particular SLF4J logging provider to use if multiple providers are available on the classpath; or
+	 *          explicitly setting the logging provider so that the classpath will not be scanned, in order to lower startup times; or supplying a provider to
+	 *          use, such as a NOP provider, if no provider is on the classpath at all.
+	 * @implSpec This implementation sets the <code>slf4j.provider</code> system property.
+	 * @implNote This implementation immediately "locks in" the new value by initializing SLF4J, so that future calls to this method will have no effect on the
+	 *           default provider actually used. This is done to suppress an output to <code>stderr</code> noting that an explicit provider is being used. This
+	 *           behavior may be removed when SLF4J discontinues sending such a message to <code>stderr</code>; see
+	 *           <a href="https://github.com/qos-ch/slf4j/issues/361">#361: Don't log to stderr when slf4j.provider is used.</a>.
+	 * @param loggingProviderClass The class of the default SLF4J logging provider to use.
+	 * @param force <code>true</code> if the preferred logging provider should override any system property already set; or <code>false</code> (preferred) to only
+	 *          set the property if it is not set already, which allows even this manual setting to still be overridden by explicitly setting the system property
+	 *          externally.
+	 * @return <code>true</code> if the system default logging provider was updated or already matched the given provider class; or <code>false</code> if the
+	 *         system default logging provider was already set and <code><var>force</var></code> was set to <code>false</code>.
+	 * @see <a href="https://jira.qos.ch/browse/SLF4J-450">SLF4J-450: Allow binding to be explicitly specified</a>
+	 */
+	public static boolean setSystemDefaultLoggingProvider(@Nonnull final Class<? extends SLF4JServiceProvider> loggingProviderClass, final boolean force) {
+		final String slf4jProviderSystemProperty = "slf4j.provider"; //TODO switch to official SLF4J constant when available; see https://github.com/qos-ch/slf4j/issues/362
+		final String loggingProviderClassName = loggingProviderClass.getName();
+		final String currentSystemDefaultLoggingProviderClassName = System.getProperty(slf4jProviderSystemProperty);
+		final boolean isChanging = !loggingProviderClassName.equals(currentSystemDefaultLoggingProviderClassName);
+		if(currentSystemDefaultLoggingProviderClassName != null && isChanging && !force) {
+			getLogger(Clogr.class).atDebug().log("System default logger provider already set to `{}`; skipping request to change to `{}`.",
+					currentSystemDefaultLoggingProviderClassName, loggingProviderClass);
+			return false;
+		}
+		if(isChanging) {
+			if(currentSystemDefaultLoggingProviderClassName != null && force) {
+				getLogger(Clogr.class).atWarn().log("Programmatically overriding system default logging provider already set to `{}` with `{}`.",
+						currentSystemDefaultLoggingProviderClassName, loggingProviderClass);
+			}
+			System.setProperty(slf4jProviderSystemProperty, loggingProviderClassName);
+		}
+		//retrieve a logger factory solely to lock-in the selected provider and suppress the notification to `stdout`
+		//see https://github.com/qos-ch/slf4j/issues/361
+		final PrintStream originalSystemErr = System.err;
+		try {
+			System.setErr(new PrintStream(nullOutputStream()));
+			LoggerFactory.getILoggerFactory();
+		} finally {
+			System.setErr(originalSystemErr);
+		}
+		return true;
 	}
 
 }
